f61dc0184385ae3ef2fc64d4d9ccfc335e129777
Mamdouh Mohammed <mmohammed@swin.edu.au>
2018-04-17 11:44:15 +1000

    Support DI

--- sampleQuartz/src/main/java/org/swinburne/test/quartz/TestJob.java
+++ sampleQuartz/src/main/java/org/swinburne/test/quartz/TestJob.java
@@ -8,16 +8,21 @@ import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestJob implements Job {
-	
-	Logger logger =  Logger.getLogger(TestJob.class);
 
-    public void execute(final JobExecutionContext ctx)
-            throws JobExecutionException {
+	Logger logger = Logger.getLogger(TestJob.class);
 
-        System.out.println("Executing Job");
-        logger.error("Executing Job");
-    }
+	public void execute(final JobExecutionContext ctx) throws JobExecutionException {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		BeanFactory factory = context;
+		Object test = factory.getBean("writer");
+
+		System.out.println("Executing Job: "+test);
+		logger.error("Executing Job");
+	}
 
 }
